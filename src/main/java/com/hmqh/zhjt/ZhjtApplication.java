701eb7050803e9006e38cbc7806383c99b766393
zhangzhixuan520 <1293124894@qq.com>
2020-05-18 17:26:58 +0800

    自己

---
+++ src/main/java/com/hmqh/zhjt/ZhjtApplication.java
@@ -0,0 +1,13 @@
+package com.hmqh.zhjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hmqh/zhjt/ZhjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmqh.zhjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
